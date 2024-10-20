0368333676c0626a49e40808a9a6514dd228c2ae
Osman Zakir <osmanzakir90@hotmail.com>
2024-10-20 16:08:17 +0500

    wrote code to convert amount to number and from dollars to cents, then revalidated cache with new data and redirect to /dashboard/invoices page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,34 @@
 "use server";
 
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string()
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export const createInvoice = async (formData: FormData) => {
-  const rawFormData = {
-    customerId: formData.get("customerId"),
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customrId"),
     amount: formData.get("amount"),
     status: formData.get("status")
-  };
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
 
-  // test it out
-  console.log(rawFormData);
+  await sql`
+    INSERT INTO invoices (customerId, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 };
